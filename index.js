ef2285fb8e88dee8381e31c456c1c56c630604ce
Anika <anika.bd1994@gmail.com>
2023-05-21 20:02:33 +0600

    another try to load data

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
     })
 
 
+    // app.get('/categoryToyDetails/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await categoryToyCollection.findOne(query);
+    //   res.send(result);
+    // });
+
+    
     app.get('/categoryToyDetails/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
